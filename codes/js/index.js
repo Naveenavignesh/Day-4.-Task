73d8464879c3297957eefcd04446a693492fba5a
Naveenavignesh <naveenanavii4437@gmail.com>
2024-05-22 04:20:10 -0700

    program

---
+++ codes/js/index.js
@@ -0,0 +1,6 @@
+let number =4;
+if(number % 2==0)
+{
+    console.log("the odd number")
+}
+ 
---
+++ codes/js/script.js
@@ -0,0 +1,8 @@
+const numbers =[22, 44, 66, 88];
+function odd__num(number){
+    for(let i=0;i<=numbers.length;i++){
+        console.log(numbers[i]);
+    }
+    
+}
+const odd_nums = odd__num(numbers)
